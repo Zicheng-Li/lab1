497e8a7536d88a621efdce281499193b6bce0cc9
Zicheng Li <103531886+Zicheng-Li@users.noreply.github.com>
2023-02-09 23:02:19 -0500

    Update bottom_up_mergesort.py

--- bottom_up_mergesort.py
+++ bottom_up_mergesort.py
@@ -1,1 +1,19 @@
-csc
+def merge(left, right):
+    L = []
+    i = j = 0
+
+    while i < len(left) or j < len(right):
+        if i >= len(left):
+            L.append(right[j])
+            j += 1
+        elif j >= len(right):
+            L.append(left[i])
+            i += 1
+        else:
+            if left[i] <= right[j]:
+                L.append(left[i])
+                i += 1
+            else:
+                L.append(right[j])
+                j += 1
+    return L
